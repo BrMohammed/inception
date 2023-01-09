c17b938cb362bc3b18bf5745f767f9ea729e1279
mohammed <togachivon@gmail.com>
2023-01-10 00:43:57 +0100

    finish mandatoty ba9i chwi

--- srcs/reuirements/mariadb/script_maria.sh
+++ srcs/reuirements/mariadb/script_maria.sh
@@ -6,6 +6,5 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS mariadb;"
 mysql -u root -e "CREATE USER IF NOT EXISTS 'von'@'%' IDENTIFIED BY '0000'"
 mysql -u root -e "GRANT ALL ON mariadb.* TO 'von'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-
-#mysql -u root -e mariadb < filename.sql
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysql -u root  mariadb < filename.sql
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '0000';"

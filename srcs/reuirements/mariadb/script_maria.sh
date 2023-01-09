1214aa6f6336619042077c23fb6bda64f6dffb61
mohammed <togachivon@gmail.com>
2023-01-09 23:55:28 +0100

    wp works

--- srcs/reuirements/mariadb/script_maria.sh
+++ srcs/reuirements/mariadb/script_maria.sh
@@ -1,26 +1,11 @@
-
 #!/bin/bash
 service mysql start
 
 # Creating a Database and User for WordPress
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
-mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS mariadb;"
+mysql -u root -e "CREATE USER IF NOT EXISTS 'von'@'%' IDENTIFIED BY '0000'"
+mysql -u root -e "GRANT ALL ON mariadb.* TO 'von'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
 
-
-#or
-
-#!/bin/bash
-
-service mysql start
-mysql -u root
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
-mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
-mysql -e "QUIT"
-
-#mysql -u wpuser -p show db
+#mysql -u root -e mariadb < filename.sql
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
--- srcs/reuirements/wordpress/wp-config.php
+++ srcs/reuirements/wordpress/wp-config.php
@@ -20,17 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mariadb' );
-
+define('DB_NAME','mariadb');
 
 /** Database username */
-define( 'DB_USER', 'br' );
+define('DB_USER','von');
 
 /** Database password */
-define( 'DB_PASSWORD', '0000' );
+define('DB_PASSWORD','0000');
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb' );
+define('DB_HOST','mariadb');
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );

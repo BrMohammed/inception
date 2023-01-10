da40c383f4c7bc3fb2ab77b32eb70947e72b90ea
mohammed <togachivon@gmail.com>
2023-01-10 01:01:09 +0100

    end mandatory

--- srcs/reuirements/mariadb/script_maria.sh
+++ srcs/reuirements/mariadb/script_maria.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 service mysql start
-
 # Creating a Database and User for WordPress
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS mariadb;"
-mysql -u root -e "CREATE USER IF NOT EXISTS 'von'@'%' IDENTIFIED BY '0000'"
-mysql -u root -e "GRANT ALL ON mariadb.* TO 'von'@'%';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root  mariadb < filename.sql
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '0000';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
